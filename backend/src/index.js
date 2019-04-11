6f2e1070d82e129c3204bf89e10c8abaa78aef79
grimgoon <alexander@hermansson.us>
2019-04-11 19:56:30 +0200

    Basic express backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const twitchRouter = require('./routers/twitch');
+const igdbRouter = require('./routers/igdb'); 
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(twitchRouter);
+app.use(igdbRouter);
+
+app.listen(port, () => {
+    console.log('Server is up on port: ' + port);
+});
+

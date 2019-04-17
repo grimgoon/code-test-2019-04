43f655b76e7036f3370d1c334af89fe0b6e707af
grimgoon <alexander@hermansson.us>
2019-04-17 21:52:22 +0200

    Styling 404-page

--- frontend/src/components/NoMatch/NoMatch.js
+++ frontend/src/components/NoMatch/NoMatch.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import style from './NoMatch.module.css';
+import {NavLink} from 'react-router-dom'
 
 const NoMatch = () => {
-    return (<div>No Match</div>);
+    return (
+        <div className={style.content}>
+            <p>The page you were looking for could not be found!</p>
+            <NavLink to="/" className={style.button}>Back to start page</NavLink>
+        </div>);
 }
 
 export default NoMatch;

393d6d2670315adf77f584c323c2d5592db9b630
grimgoon <alexander@hermansson.us>
2019-04-18 11:12:23 +0200

    searchbar update to push to /search route & GET-param

--- frontend/src/components/UI/SearchBar/SearchBar.js
+++ frontend/src/components/UI/SearchBar/SearchBar.js
@@ -1,18 +1,32 @@
 import React, {Component} from 'react';
 import style from './SearchBar.module.css';
+import {withRouter} from 'react-router-dom';
 
 class SearchBar extends Component {
+
+    state = {
+        searchValue : null
+    }
+
     submit = (e) => {
         e.preventDefault();
+        let searchValue = this.state.searchValue;
+        let path = '/search/?value=' + searchValue;
+        this.props.history.push({pathname : path});
     };
 
+    searchValueHandler = (e) => {
+        this.setState({searchValue : e.target.value});
+    }
+
     render() {
+        console.log(this.props);
         return (
             <form onSubmit={this.submit}>
-                <input type="text" placeholder="Search..." className={style.SearchBar}/>
+                <input type="text" placeholder="Search..." value={this.state.searchValue} onChange={this.searchValueHandler} className={style.SearchBar}/>
             </form>
         );
     };
 };
 
-export default SearchBar;
+export default withRouter(SearchBar);
